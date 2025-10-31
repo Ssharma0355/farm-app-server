6583216668f3ce52fea758be15ee08cccaa45e95
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-10-31 18:40:00 +0530

    added cors to check in frontend

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+# Optional: Allow frontend (React) access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-def home():
-    return{"message":"Hi sachin"}
+def initial():
+    return{
+        "message":"Hi sachin"
+        }
